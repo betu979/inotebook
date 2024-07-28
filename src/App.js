40da37f245b87ea01eed3dc23b14c5ebfcd4ce2e
abhilash <abhilash.chaurasiya123@gmail.com>
2024-07-28 22:33:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn iNotebook
         </a>
       </header>
     </div>
